793a74aca02efbe3f5884968137b605e6ef5c751
Josh Mu <hello@joshmu.dev>
2025-01-31 18:07:30 +1000

    feat: add docker (dozzle)

--- src/components/Server/Server.tsx
+++ src/components/Server/Server.tsx
@@ -3,6 +3,7 @@ export const Server = () => (
       <h1>- DRK.GDN -</h1>
       <ul style={{padding: 0, marginTop: '20px', listStyle: 'none', fontFamily: 'monospace', fontSize: '1.2rem', textAlign: 'center'}}>
         <li><a href='https://stats.drk.gdn'>stats</a></li>
+        <li><a href='https://docker.drk.gdn'>docker</a></li>
         <li><a href='https://console.storage.drk.gdn'>storage</a></li>
         <li><a href='https://supabase.drk.gdn'>supabase</a></li>
         <li><a href='https://langfuse.drk.gdn'>langfuse</a></li>
