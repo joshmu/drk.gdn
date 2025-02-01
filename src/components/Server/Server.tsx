e2f93994507fdca3cd98007b8463a6379838f121
Josh Mu <hello@joshmu.dev>
2025-02-01 14:02:27 +1000

    fix(storage): update endpoint to resolve SSL issue

--- src/components/Server/Server.tsx
+++ src/components/Server/Server.tsx
@@ -4,7 +4,7 @@ export const Server = () => (
       <ul style={{padding: 0, marginTop: '20px', listStyle: 'none', fontFamily: 'monospace', fontSize: '1.2rem', textAlign: 'center'}}>
         <li><a href='https://stats.drk.gdn'>stats</a></li>
         <li><a href='https://docker.drk.gdn'>docker</a></li>
-        <li><a href='https://console.storage.drk.gdn'>storage</a></li>
+        <li><a href='https://storage.drk.gdn'>storage</a></li>
         <li><a href='https://supabase.drk.gdn'>supabase</a></li>
         <li><a href='https://langfuse.drk.gdn'>langfuse</a></li>
         <li><a href='https://n8n.drk.gdn'>n8n</a></li>
